78cd7ac5574bb452e24dff11c5ca33f8fa8e5537
MARCELNAKA <46759557+MARCELNAKA@users.noreply.github.com>
2019-02-11 21:22:58 -0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+# Homework1 
+
+#detais
+Band = "The Killers"
+Genre = "Indie Rock"
+Discs = 5
+
+Print (Band)
+Print (Genre)
+Print (Discs)
+
+Band = "Coldplay"
+
+Print (Band)
+#shoud be Coldplay
+
+Discs = Discs + 1
+Print (Discs)
+#should be 6
+
+Discs *=2
+Print (Discs)
+#should be 12
+
+Discs /=3
+Print (Discs)
+#shoud be 4
