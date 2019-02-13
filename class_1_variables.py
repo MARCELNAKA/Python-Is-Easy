d0eeec8239a174dee13722db5c9b642c81bd3d46
MARCELNAKA <46759557+MARCELNAKA@users.noreply.github.com>
2019-02-13 07:23:24 -0200

    Add files via upload

---
+++ class_1_variables.py
@@ -0,0 +1,29 @@
+"""
+Homework1
+"""
+
+#variables
+Band = "The Killers"
+Genre = "Indie Rock"
+Discs = 5
+
+Print (Band)
+Print (Genre)
+Print (Discs)
+
+Band = "Coldplay"
+
+Print (Band)
+#shoud be Coldplay
+
+Discs = Discs + 1
+Print (Discs)
+#should be 6
+
+Discs *=2
+Print (Discs)
+#should be 12
+
+Discs /=3
+Print (Discs)
+#shoud be 4
