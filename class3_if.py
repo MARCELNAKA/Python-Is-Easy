19751f2681f0c9c0fa31a0bf6d7720881d1b016a
MARCELNAKA <46759557+MARCELNAKA@users.noreply.github.com>
2019-02-15 18:39:58 -0200

    Add files via upload

---
+++ class3_if.py
@@ -0,0 +1,33 @@
+baskTeam = "celtics"
+soccerTeam = "corinthians"
+baseTeam = "yankees"
+
+if baskTeam = "celtics" and soccerTeam = "corinthians"
+    return = True
+
+elif baskTeam = "celtics" and baseTeam = "yankees"
+    return = True
+
+elif soccerTeam = "corinthians" and baseTeam = "yankees"
+    return = True
+
+else
+    return = False
+
+print (return)
+
+#extra
+
+a = "5"
+output = True
+a = int(a)
+
+#print (a)
+
+if a==5:
+    output = True
+
+else:
+    output = False
+
+print (output)
